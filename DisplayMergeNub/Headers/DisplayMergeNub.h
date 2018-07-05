38922853cc41fb91978df0a9be9e65a5778407b3
FioneraGH <fionera@outlook.com>
2018-07-05 11:03:32 +0800

    minor fix

--- DisplayMergeNub/Headers/DisplayMergeNub.h
+++ DisplayMergeNub/Headers/DisplayMergeNub.h
@@ -8,8 +8,8 @@ class DisplayMergeNub : public IOService
     OSDeclareDefaultStructors(DisplayMergeNub)
     
 public:
-    IOService *			probe(IOService *provider, SInt32 *score);
-    bool                start(IOService *provider);
+    IOService *			probe(IOService *provider, SInt32 *score) override;
+    bool                start(IOService *provider) override;
     virtual bool 		MergeDictionaryIntoProvider(IOService *  provider, OSDictionary *  mergeDict);
     virtual bool		MergeDictionaryIntoDictionary(OSDictionary *  sourceDictionary,  OSDictionary *  targetDictionary);
 
